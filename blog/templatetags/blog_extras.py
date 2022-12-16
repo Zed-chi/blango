479ea9929ed817c11e08fa5328f0b74f4299cdf7
Alexandr Efimov <zed-chi@mail.ru>
2022-12-16 22:09:21 +0000

    Finished custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,6 +3,8 @@ from django.template import Library
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
+from blog.models import Post
+
 
 register = Library()
 
@@ -19,3 +21,29 @@ def author_details(author:User, current_user:User):
   if author.email:
     return format_html('<a href="mailto:{}">{}</a>', author.email, name)    
   return mark_safe(escape(name))
+
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+
+@register.simple_tag
+def endrow():
+  return format_html("</div>")
+
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+  
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts, "fuck":"dddd"}
